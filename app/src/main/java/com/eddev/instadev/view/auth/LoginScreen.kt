5783793b2b07a3c7fae653e4c45ca1847f87a045
EdgarNunezTrejo <evil_bart1@hotmail.com>
2025-08-02 09:48:53 +0200

    feat: navigation creation

---
+++ app/src/main/java/com/eddev/instadev/view/auth/LoginScreen.kt
@@ -0,0 +1,9 @@
+package com.eddev.instadev.view.auth
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier) {
+    
+}
---
+++ app/src/main/java/com/eddev/instadev/view/core/navigation/NavigationWrapper.kt
@@ -0,0 +1,16 @@
+package com.eddev.instadev.view.core.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun NavigationWrapper() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Login){
+        composable<Login> {
+
+        }
+    }
+}
